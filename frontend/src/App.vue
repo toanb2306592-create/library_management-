db3d5731e5e78f4ef14e608743e69dbc793f51c7
toanb2306592-create <toanb2306592@student.ctu.edu.vn>
2025-11-28 19:56:39 +0700

    up

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+
+
+export default {
+  components: {
+    AppHeader,
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <AppHeader />
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>

ad8f161d31c50639156d4efbf84b13099053a28d
toanb2306592-create <toanb2306592@student.ctu.edu.vn>
2025-11-28 18:05:23 +0700

    up

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-const express = require("express");
-const cors = require("cors");
+const express = require("express"); 
+const cors = require("cors"); 
+const ApiError = require("./app/api-error");
 
-const app = express();
+const app = express(); 
 
-app.use(cors());
-app.use(express.json());
+const contactsRouter = require("./app/routes/contact.route");
 
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to contact book application." });
+app.use(cors()); 
+app.use(express.json()); 
+app.use("/api/contacts", contactsRouter); 
+
+app.get("/", (req, res) => { 
+    res.json({ message: "Welcome to contact book application." }); 
+}); 
+
+app.use((req, res, next) => {
+
+    return next(new ApiError(404, "Resource not found"));
 });
 
-module.exports = app;
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
+
+
+module.exports = app; 
